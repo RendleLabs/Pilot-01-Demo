e74a6ed620f29f73e537d2e47e7a53b73a836b2e
Mark Rendle <mark@rendlelabs.com>
2017-08-13 17:56:51 +0100

    Initial commit

---
+++ src/DockerDemo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DockerDemo.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DockerDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View(SystemInfoViewModel.FromEnvironment(_hostingEnvironment));
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/DockerDemo/Models/SystemInfoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace DockerDemo.Models
+{
+    public class SystemInfoViewModel
+    {
+        public static SystemInfoViewModel FromEnvironment(IHostingEnvironment hostingEnvironment)
+        {
+            var vm = new SystemInfoViewModel
+            {
+                OSVersion = Environment.OSVersion.VersionString,
+                MachineName = Environment.MachineName,
+                ProcessorCount = Environment.ProcessorCount,
+                ClrVersion = Environment.Version.ToString(),
+                RuntimeType = PlatformServices.Default.Application.RuntimeFramework.FullName,
+                EnvironmentName = hostingEnvironment.EnvironmentName
+            };
+            return vm;
+        }
+
+        public string ClrVersion { get; private set; }
+        public int ProcessorCount { get; private set; }
+        public string MachineName { get; private set; }
+        public string OSVersion { get; private set; }
+        public string RuntimeType { get; private set; }
+        public string EnvironmentName { get; private set; }
+    }
+}
